4f59e64020748fb758b861736712d3cbce7c20d8
tizianatortone <tiziana_tortone@hotmail.com>
2021-02-04 14:38:41 +0100

    removed <a> without real url & updated github url

--- src/Github.js
+++ src/Github.js
@@ -4,7 +4,7 @@ import "./Github.css";
 export default function Github() {
   return (
     <div className="Github">
-            <p><a href="https://github.com/tizianatortone/weather-app" target="_blank" rel="noreferrer"> Open-source Code </a>
+            <p><a href="https://github.com/tizianatortone/app-weather" target="_blank" rel="noreferrer"> Open-source Code </a>
               by Tiziana Tortone</p>
         </div>
     
